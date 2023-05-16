1689daa7655f1ad89a227869d84336c1d19ad34a
Abha Meshram <abha25meshram@gmail.com>
2023-05-16 23:16:13 +0530

    add new

--- model/Admin.Model.js
+++ model/Admin.Model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const adminSchema = mongoose.Schema({
-    name:String,
-    image:String,
-    email:String,
-    password:String,
-    gender:String,
-    phoneNumber:Number
+    name: String,
+    image: String,
+    email: String,
+    password: String,
+    gender: String,
+    phoneNumber: Number
 })
 
 const AdminModel = mongoose.model("admin", adminSchema)
